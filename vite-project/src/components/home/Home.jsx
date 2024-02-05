19d534ae6669bb2aa852206fbae085270b68cc8e
AHMDMUHSIN <zanmuhsin11@gmail.com>
2024-02-05 20:47:30 +0530

    added

--- vite-project/src/components/home/Home.jsx
+++ vite-project/src/components/home/Home.jsx
@@ -326,7 +326,7 @@ const Home = () => {
             </linearGradient>
             <radialGradient id="paint1_radial_1366_4582" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(60 60) rotate(96.8574) scale(122.674 149.921)">
               <stop stop-color="#87A1FF"></stop>
-              <stop offset="1" stop-color="#16161D" stop-opacity="0.2"></stop>
+              <stop offset="1" stop-color="#16161D" ></stop>
             </radialGradient>
           </defs>
         </svg>
